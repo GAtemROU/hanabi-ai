1fe09ca0078fa6ef5d3e0bd455db572eb5eed9a0
tymy00001 <tymy00001@stud.uni-saarland.de>
2024-09-06 21:44:58 +0200

    clean up

--- run_game.py
+++ run_game.py
@@ -21,9 +21,9 @@ env.set_agents([agent for _ in range(env.num_players)])
 trajectories, score = env.run(is_training=True)
 
 print('\nTrajectories:')
-print(trajectories)
+# print(trajectories)
 print('\nSample raw observation:')
-pprint.pprint(trajectories[0][0]['raw_obs'])
+# pprint.pprint(trajectories[0][0]['raw_obs'])
 print('\nSample raw legal_actions:')
-pprint.pprint(trajectories[0][0]['raw_legal_actions'])
+# pprint.pprint(trajectories[0][0]['raw_legal_actions'])
 
